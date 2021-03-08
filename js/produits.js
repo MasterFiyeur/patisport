55b14d6d3d4b07484aeef9f5335a2cc170dd855d
Théo <julientheo@eisti.eu>
2021-03-08 13:27:13 +0100

    js sur le premier produit des proteges

--- js/produits.js
+++ js/produits.js
@@ -1,5 +1,4 @@
-function stock_switch(e){
-    console.log(this);
+function stock_switch(){
     var src_image = document.getElementById("toggle_stock");
     if(src_image.getAttribute("src")=="./img/produits/show.svg"){
 
@@ -7,5 +6,37 @@ function stock_switch(e){
     }else{
 
         src_image.setAttribute("src","./img/produits/show.svg");
+    }
+}
+
+function add_stock(forms_nombre){
+    let nombre = parseInt(document.forms[forms_nombre].nombre_produit.value);
+    let max_nombre = parseInt(document.getElementsByClassName("stock")[forms_nombre].textContent.substring(8));
+    if(nombre<max_nombre){
+        nombre +=1;
+        document.forms[forms_nombre].nombre_produit.value = nombre;
+        if(nombre==1){
+            document.getElementsByClassName("moins_stock")[forms_nombre].style.pointerEvents = "auto";
+        }
+    }
+    if(nombre==max_nombre){
+        console.log("maximum");
+        document.getElementsByClassName("plus_stock")[forms_nombre].style.pointerEvents = "none";
+    }
+}
+
+function del_stock(forms_nombre){
+    let nombre = parseInt(document.forms[forms_nombre].nombre_produit.value);
+    let max_nombre = parseInt(document.getElementsByClassName("stock")[forms_nombre].textContent.substring(8));
+    if(nombre>0){
+        nombre -=1;
+        document.forms[forms_nombre].nombre_produit.value = nombre;
+        if(nombre == max_nombre-1){
+            document.getElementsByClassName("plus_stock")[forms_nombre].style.pointerEvents = "auto";
+        }
+    }
+    if(nombre==0){
+        console.log("minimum");
+        document.getElementsByClassName("moins_stock")[forms_nombre].style.pointerEvents = "none";
     }
 }
