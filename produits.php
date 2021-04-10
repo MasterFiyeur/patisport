ae99b4b3b6135c93f4557de1737585616c16a807
Théo <julientheo@eisti.eu>
2021-04-10 15:51:09 +0200

    produit compact

--- produits.php
+++ produits.php
@@ -35,32 +35,36 @@
                         if(isset($_SESSION["produits"][$categorie])){
                             /* Utilisation du for car besoin d'un indice $i */
                             for ($i=0; $i < count($_SESSION["produits"][$categorie]); $i++) {
-                                echo "<div class='carte'>
+                            ?>
+                                <div class='carte'>
                                     <div class='imageBox'>
-                                        <img ".$_SESSION["produits"][$categorie][$i]["img"].">
+                                        <img <?php echo $_SESSION["produits"][$categorie][$i]["img"]; ?>>
                                     </div>
                                     <div class='contentBox'>
-                                        <h3>".$_SESSION["produits"][$categorie][$i]["label"]." <small>(".$_SESSION["produits"][$categorie][$i]["ref"].")</small></h3>
-                                        <h2 class='prix'>€".$_SESSION["produits"][$categorie][$i]["prix"]."</h2>
+                                        <h3><?php echo $_SESSION["produits"][$categorie][$i]["label"]; ?>
+                                            <small>(<?php echo $_SESSION["produits"][$categorie][$i]["ref"]; ?>)</small>
+                                        </h3>
+                                        <h2 class='prix'>€<?php echo $_SESSION["produits"][$categorie][$i]["prix"]; ?></h2>
                                         <div class='hidden_contentBox'>
                                             <form action='' method='POST'>
-                                                <div class='stock'>Stock : ".stockDisponible(array($categorie,$i,0))."</div>
+                                                <div class='stock'>Stock : <?php echo stockDisponible(array($categorie,$i,0)); ?></div>
                                                 <div class='gestion_stock'>
-                                                    <input type='hidden' name='categorie' value='".$categorie."'>
-                                                    <input type='hidden' name='index' value='".$i."'>
-                                                    <input type='button' class='moins_stock' onclick='del_stock(".($i+1).")' value='-'>
+                                                    <input type='hidden' name='categorie' value=<?php echo "'".$categorie."'"; ?>>
+                                                    <input type='hidden' name='index' value= <?php echo "'".$i."'"; ?>>
+                                                    <input type='button' class='moins_stock' <?php echo "onclick='del_stock(".($i+1).")'"; ?> value='-'>
                                                     <input type='text' name='nombre_produit' id='nombre_produit' value='0'>
-                                                    <input type='button' class='plus_stock' onclick='add_stock(".($i+1).")' value='+'>
+                                                    <input type='button' class='plus_stock' <?php echo "onclick='add_stock(".($i+1).")'"; ?> value='+'>
                                                 </div>
                                                 <input type='submit' class='btn_acheter' value='Acheter'>
                                             </form>
                                         </div>
                                     </div>
-                                </div>";
-                            }
-                        }else{
-                            echo "<div>Il n'y a aucun produit disponible pour cette catégorie</div>";
-                        }
+                                </div>
+                            <?php }
+                        }else{ 
+                            ?>
+                            <div>Il n'y a aucun produit disponible pour cette catégorie</div>
+                    <?php }
                     ?>
                 </div>
                 <button class="btn_stock-switch" onclick="stock_switch()"><img id="toggle_stock" src="./img/produits/show.svg"></button>
